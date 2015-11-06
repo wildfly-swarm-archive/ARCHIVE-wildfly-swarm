df70a3ec319de9cbbe85f177bdccc0dfc7633cdb
John D. Ament <johndament@apache.org>
2015-11-05 23:29:36 -0500

    Removed unnecessary config objects, they're generated now.

--- webservices/api/src/main/java/org/wildfly/swarm/webservices/WebServicesFraction.java
+++ webservices/api/src/main/java/org/wildfly/swarm/webservices/WebServicesFraction.java
@@ -16,59 +16,9 @@
 
 package org.wildfly.swarm.webservices;
 
+import org.wildfly.swarm.config.Webservices;
 import org.wildfly.swarm.container.Fraction;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
-public class WebServicesFraction implements Fraction {
-    private HostInfo hostInfo = new HostInfo();
-    private List<EndpointConfig> endpointConfigs = new ArrayList<>();
-    private List<ClientConfig> clientConfigs = new ArrayList<>();
-    public WebServicesFraction() {
-        this(new HostInfo());
-    }
-
-    public WebServicesFraction(HostInfo hostInfo, Collection<EndpointConfig> endpointConfigs, Collection<ClientConfig> clientConfigs) {
-        if(hostInfo != null) {
-            this.hostInfo = hostInfo;
-        }
-        this.endpointConfigs.addAll(endpointConfigs);
-        this.clientConfigs.addAll(clientConfigs);
-    }
-
-    public WebServicesFraction(HostInfo hostInfo) {
-        this(hostInfo, Arrays.asList(EndpointConfig.STANDARD,EndpointConfig.RECORDING), Collections.singleton(ClientConfig.STANDARD));
-    }
-
-    public WebServicesFraction hostInfo(HostInfo hostInfo) {
-        this.hostInfo = hostInfo;
-        return this;
-    }
-
-    public WebServicesFraction endpointConfig(EndpointConfig endpointConfig) {
-        this.endpointConfigs.add(endpointConfig);
-        return this;
-    }
-
-    public WebServicesFraction clientConfig(ClientConfig clientConfig) {
-        this.clientConfigs.add(clientConfig);
-        return this;
-    }
-
-    public Collection<EndpointConfig> getEndpointConfigs() {
-        return Collections.unmodifiableCollection(endpointConfigs);
-    }
-
-    public Collection<ClientConfig> getClientConfigs() {
-        return Collections.unmodifiableCollection(clientConfigs);
-    }
-
-    public HostInfo getHostInfo() {
-        return hostInfo;
-    }
+public class WebServicesFraction extends Webservices<WebServicesFraction> implements Fraction {
 
 }
--- webservices/runtime/src/main/java/org/wildfly/swarm/webservices/runtime/WebServicesConfiguration.java
+++ webservices/runtime/src/main/java/org/wildfly/swarm/webservices/runtime/WebServicesConfiguration.java
@@ -17,11 +17,17 @@
 package org.wildfly.swarm.webservices.runtime;
 
 import org.jboss.dmr.ModelNode;
+import org.wildfly.swarm.config.runtime.invocation.Marshaller;
 import org.wildfly.swarm.container.runtime.AbstractServerConfiguration;
 import org.wildfly.swarm.webservices.WebServicesFraction;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.EXTENSION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
 
 public class WebServicesConfiguration extends AbstractServerConfiguration<WebServicesFraction> {
     public WebServicesConfiguration() {
@@ -34,7 +40,16 @@ public class WebServicesConfiguration extends AbstractServerConfiguration<WebSer
     }
 
     @Override
-    public List<ModelNode> getList(WebServicesFraction fraction) {
-        return new ArrayList<>();
+    public List<ModelNode> getList(WebServicesFraction fraction) throws Exception {
+        List<ModelNode> list = new ArrayList<>();
+
+        ModelNode node = new ModelNode();
+        node.get(OP_ADDR).set(EXTENSION, "org.jboss.as.webservices");
+        node.get(OP).set(ADD);
+        list.add(node);
+
+        list.addAll(Marshaller.marshal(fraction));
+
+        return list;
     }
 }
