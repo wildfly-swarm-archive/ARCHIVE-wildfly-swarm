7cc8006f3a1bac441f965d34bd1d780c5bfc85e7
Lance Ball <lball@redhat.com>
2016-01-25 10:25:53 -0500

    Provide a swagger-webapp fraction.
    Motivation:
    We already provide a swagger fraction that allows developers to expose
    their APIs via /swagger.json. The `swagger-webapp` fraction provides a
    simple way to expose the Swagger UI http://swagger.io/swagger-ui/ as
    well.
    
    The UI may be run as its own standalone deployment, or it may be
    included as a part of another deployment.
    
    Modifications:
    Added the `swagger-webapp` fraction. Adjusted /pom.xml and the bom
    accordingly.
    
    Result:
    Currently the UI seems to be working in the simple InVM test, but not in
    the Arquillian test. This commit is a checkpoint.

---
+++ swagger-webapp/runtime/src/main/java/org/wildfly/swarm/swagger/webapp/runtime/SwaggerWebAppConfiguration.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2015-2016 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.swarm.swagger.webapp.runtime;
+
+import org.jboss.shrinkwrap.api.Archive;
+import org.wildfly.swarm.container.runtime.AbstractServerConfiguration;
+import org.wildfly.swarm.swagger.webapp.SwaggerProperties;
+import org.wildfly.swarm.swagger.webapp.SwaggerWebAppFraction;
+import org.wildfly.swarm.undertow.WARArchive;
+
+
+/**
+ * @author Lance Ball
+ */
+public class SwaggerWebAppConfiguration extends AbstractServerConfiguration<SwaggerWebAppFraction> {
+
+    private static final String DEFAULT_CONTEXT = "/swagger-ui";
+
+    public SwaggerWebAppConfiguration() {
+        super(SwaggerWebAppFraction.class);
+
+        deployment("org.wildfly.swarm:swagger-webapp-ui:war:1.0.0.Alpha7-SNAPSHOT")
+                .as("swagger-webapp-ui.war")
+                .configure((fraction, archive) -> {
+                    archive.as(WARArchive.class).setContextRoot(getContext());
+                });
+    }
+
+    @Override
+    public SwaggerWebAppFraction defaultFraction() {
+        return new SwaggerWebAppFraction();
+    }
+
+    private static String getContext() {
+        String context = System.getProperty(SwaggerProperties.CONTEXT_PATH);
+        if (context == null || context == "") context = DEFAULT_CONTEXT;
+        return context;
+    }
+}
